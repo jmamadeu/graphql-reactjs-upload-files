a12fccdc415b9c4ea1213ba63fb391a4ab13518d
jmamadeu <jmamadeu2000@gmail.com>
2021-01-17 17:18:44 +0100

    :sparkles: Configure tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './pages/**/*.js',
+    './components/**/*.js',
+    './pages/**/*.ts',
+    './components/**/*.ts',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
